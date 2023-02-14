9bf06b38f0e08e402be7bf54358eb02b50060571
Sergey Kalachev <rollstream@yandex.ru>
2023-02-15 01:46:16 +0700

    fixed after the review

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('./dist/'))
 
-app.listen(port, () => {
-    console.log(`App started on ${port}`)
+app.listen(PORT, () => {
+    console.log(`App started on ${PORT}`)
 })
